8a9606a418d9d3b7d1ebccdbce0dbf9bffe54110
faisalinitiate <faisalcongrates@gmail.com>
2025-08-12 15:40:25 +0600

    Update MultiBanner.jsx

--- src/components/MultiBanner.jsx
+++ src/components/MultiBanner.jsx
@@ -3,20 +3,88 @@ import BImgOne from '../assets/BImgOne.jpg'
 
 const MultiBanner = () => {
   return (
-    <div className='max-w-6xl mx-auto border-2 border-solid border-slate-500 mt-24 mb-12 grid grid-cols-4'>
-      
-        <div className="col-span-1 border-2 border-solid border-black bg-amber-300">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus architecto error at, odit laborum illum beatae aspernatur veniam ipsa molestiae.</div>
-        <div className="col-span-2 border-2 border-solid border-black bg-amber-300">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus architecto error at, odit laborum illum beatae aspernatur veniam ipsa molestiae.</div>
-        <div className="col-span-1 border-2 border-solid border-black bg-amber-300">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus architecto error at, odit laborum illum beatae aspernatur veniam ipsa molestiae.</div>
-
-        <div className="col-span-1 border-2 border-solid border-black bg-amber-300">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus architecto error at, odit laborum illum beatae aspernatur veniam ipsa molestiae.</div>
-        <div className="col-span-1 border-2 border-solid border-black bg-amber-300">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus architecto error at, odit laborum illum beatae aspernatur veniam ipsa molestiae.</div>
-        <div className="col-span-2 border-2 border-solid border-black bg-amber-300">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus architecto error at, odit laborum illum beatae aspernatur veniam ipsa molestiae.</div>
-        
-        
-        <div className="col-span-2 border-2 border-solid border-black bg-amber-300">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus architecto error at, odit laborum illum beatae aspernatur veniam ipsa molestiae.</div>
-        <div className="col-span-1 border-2 border-solid border-black bg-amber-300">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus architecto error at, odit laborum illum beatae aspernatur veniam ipsa molestiae.</div>
-        <div className="col-span-1 border-2 border-solid border-black bg-amber-300">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus architecto error at, odit laborum illum beatae aspernatur veniam ipsa molestiae.</div>
+    <div className='max-w-7xl mx-auto border-2 border-solid border-slate-500 mt-24 mb-12 grid grid-cols-7 gap-1'>
+  {/* First Image */}
+      <div className="col-span-2 h-64 border-2 border-solid border-black bg-amber-300 relative">
+        <img src={BImgOne} className="w-[100%] h-[100%]" alt="alt" />
+        <div className="border-2 border-solid border-green-400 grid place-content-center absolute inset-0">
+          <h1 className='my-1 text-white font-extrabold font-stretch-extra-expanded tracking-[0.1rem] uppercase'>Just Arrived</h1>
+          <button className='border-2 border-solid border-black bg-purple-700 rounded-lg font-bold'>Shop Now</button>
+        </div>
+        <div className="bg-zinc-900 opacity-20 absolute top-0 left-0 w-[100%] h-[100%]"></div>
+      </div>
+  {/* Second Image */}
+      <div className="col-span-3 h-64 border-2 border-solid border-black bg-amber-300 relative">
+        <img src={BImgOne} className="w-[100%] h-[100%]" alt="alt" />
+        <div className="border-2 border-solid border-green-400 grid place-content-center absolute inset-0">
+          <h1 className='my-1 text-white font-extrabold font-stretch-extra-expanded tracking-[0.1rem] uppercase'>Just Arrived</h1>
+          <button className='border-2 border-solid border-black bg-purple-700 rounded-lg font-bold'>Shop Now</button>
+        </div>
+        <div className="bg-zinc-900 opacity-20 absolute top-0 left-0 w-[100%] h-[100%]"></div>
+      </div>
+        {/* Third Image */}
+      <div className="col-span-2 h-64 border-2 border-solid border-black bg-amber-300 relative">
+        <img src={BImgOne} className="w-[100%] h-[100%]" alt="alt" />
+        <div className="border-2 border-solid border-green-400 grid place-content-center absolute inset-0">
+          <h1 className='my-1 text-white font-extrabold font-stretch-extra-expanded tracking-[0.1rem] uppercase'>Just Arrived</h1>
+          <button className='border-2 border-solid border-black bg-purple-700 rounded-lg font-bold'>Shop Now</button>
+        </div>
+        <div className="bg-zinc-900 opacity-20 absolute top-0 left-0 w-[100%] h-[100%]"></div>
+      </div>
+        {/* Fourth Image */}
+      <div className="col-span-3 h-64 border-2 border-solid border-black bg-amber-300 relative">
+        <img src={BImgOne} className="w-[100%] h-[100%]" alt="alt" />
+        <div className="border-2 border-solid border-green-400 grid place-content-center absolute inset-0">
+          <h1 className='my-1 text-white font-extrabold font-stretch-extra-expanded tracking-[0.1rem] uppercase'>Just Arrived</h1>
+          <button className='border-2 border-solid border-black bg-purple-700 rounded-lg font-bold'>Shop Now</button>
+        </div>
+        <div className="bg-zinc-900 opacity-20 absolute top-0 left-0 w-[100%] h-[100%]"></div>
+      </div>
+           {/* Fifth Image */}
+      <div className="col-span-2 h-64 border-2 border-solid border-black bg-amber-300 relative">
+        <img src={BImgOne} className="w-[100%] h-[100%]" alt="alt" />
+        <div className="border-2 border-solid border-green-400 grid place-content-center absolute inset-0">
+          <h1 className='my-1 text-white font-extrabold font-stretch-extra-expanded tracking-[0.1rem] uppercase'>Just Arrived</h1>
+          <button className='border-2 border-solid border-black bg-purple-700 rounded-lg font-bold'>Shop Now</button>
+        </div>
+        <div className="bg-zinc-900 opacity-20 absolute top-0 left-0 w-[100%] h-[100%]"></div>
+      </div>
+          {/* Sixth Image */}
+      <div className="col-span-2 h-64 border-2 border-solid border-black bg-amber-300 relative">
+        <img src={BImgOne} className="w-[100%] h-[100%]" alt="alt" />
+        <div className="border-2 border-solid border-green-400 grid place-content-center absolute inset-0">
+          <h1 className='my-1 text-white font-extrabold font-stretch-extra-expanded tracking-[0.1rem] uppercase'>Just Arrived</h1>
+          <button className='border-2 border-solid border-black bg-purple-700 rounded-lg font-bold'>Shop Now</button>
+        </div>
+        <div className="bg-zinc-900 opacity-20 absolute top-0 left-0 w-[100%] h-[100%]"></div>
+      </div>
+          {/* Sixth Image ongoing*/}
+      <div className="col-span-2 h-64 border-2 border-solid border-black bg-amber-300 relative">
+        <img src={BImgOne} className="w-[100%] h-[100%]" alt="alt" />
+        <div className="border-2 border-solid border-green-400 grid place-content-center absolute inset-0">
+          <h1 className='my-1 text-white font-extrabold font-stretch-extra-expanded tracking-[0.1rem] uppercase'>Just Arrived</h1>
+          <button className='border-2 border-solid border-black bg-purple-700 rounded-lg font-bold'>Shop Now</button>
+        </div>
+        <div className="bg-zinc-900 opacity-20 absolute top-0 left-0 w-[100%] h-[100%]"></div>
+      </div>
+          {/* Sixth Image ongoing*/}
+      <div className="col-span-2 h-64 border-2 border-solid border-black bg-amber-300 relative">
+        <img src={BImgOne} className="w-[100%] h-[100%]" alt="alt" />
+        <div className="border-2 border-solid border-green-400 grid place-content-center absolute inset-0">
+          <h1 className='my-1 text-white font-extrabold font-stretch-extra-expanded tracking-[0.1rem] uppercase'>Just Arrived</h1>
+          <button className='border-2 border-solid border-black bg-purple-700 rounded-lg font-bold'>Shop Now</button>
+        </div>
+        <div className="bg-zinc-900 opacity-20 absolute top-0 left-0 w-[100%] h-[100%]"></div>
+      </div>
+          {/* Sixth Image ongoing*/}
+      <div className="col-span-3 h-64 border-2 border-solid border-black bg-amber-300 relative">
+        <img src={BImgOne} className="w-[100%] h-[100%]" alt="alt" />
+        <div className="border-2 border-solid border-green-400 grid place-content-center absolute inset-0">
+          <h1 className='my-1 text-white font-extrabold font-stretch-extra-expanded tracking-[0.1rem] uppercase'>Just Arrived</h1>
+          <button className='border-2 border-solid border-black bg-purple-700 rounded-lg font-bold'>Shop Now</button>
+        </div>
+        <div className="bg-zinc-900 opacity-20 absolute top-0 left-0 w-[100%] h-[100%]"></div>
+      </div>
       
     </div>
   )

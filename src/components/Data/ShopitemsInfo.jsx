416e63abbba4442e2773336dc0bd6d7634a69e6c
faisalinitiate <faisalcongrates@gmail.com>
2025-08-14 11:49:29 +0600

    Create and Update Files

---
+++ src/components/Data/ShopitemsInfo.jsx
@@ -0,0 +1,43 @@
+export const ShopitemsInfo = [
+  {
+   details: "T-Shirt",
+   offer: "10% Off"   
+  },
+  {
+   details: "T-Shirt",
+   offer: "5% Off"   
+  },
+  {
+   details: "T-Shirt",
+   offer: "15% Off"   
+  },
+  {
+   details: "T-Shirt",
+   offer: "10% Off"   
+  },
+  {
+   details: "T-Shirt",
+   offer: "5% Off"   
+  },
+  {
+   details: "T-Shirt",
+   offer: "15% Off"   
+  },
+  {
+   details: "T-Shirt",
+   offer: "10% Off"   
+  },
+  {
+   details: "T-Shirt",
+   offer: "5% Off"   
+  },
+  {
+   details: "T-Shirt",
+   offer: "15% Off"   
+  },
+  {
+   details: "T-Shirt",
+   offer: "7% Off"   
+  }
+  
+]

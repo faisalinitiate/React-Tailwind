93b8fea5bedd2670131398dbbdee2004be419a18
faisalinitiate <faisalcongrates@gmail.com>
2025-08-14 16:53:18 +0600

    Update HeroBanner.jsx

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Banner from '../assets/HeroBanner.png'
+import Banner from '../assets/heroBanner.png'
 
 
 const HeroBanner = () => {
@@ -11,3 +11,17 @@ const HeroBanner = () => {
 }
 
 export default HeroBanner
+
+// import React from 'react'
+// import Newbanner from '../assets/herobannernew.png'
+
+
+// const HeroBanner = () => {
+//   return (
+//     <div className='max-w-4xl mx-auto h-120 bg-gray-400 md:rounded-2xl mt-20 flex justify-center items-center'>
+//         <div className="w-3xl  h-100 mix-blend-multiply bg-linear-to-bl from-indigo-500 to-purple-400 border-solid border-2 border-slate-500 md:rounded-2xl bg-no-repeat" style={{backgroundImage: `url(${Newbanner})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}></div>
+//     </div>
+//   )
+// }
+
+// export default HeroBanner

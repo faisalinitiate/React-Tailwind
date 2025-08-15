45429e0b2c7c9eae8b4521802a26eaff7c4e6aad
faisalinitiate <faisalcongrates@gmail.com>
2025-08-15 17:06:10 +0600

    Create and Update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import MultiBanner from '../components/MultiBanner.jsx'
 import HeroBanner from '../components/HeroBanner.jsx'
 import ShopItems from '../components/ShopItems.jsx'
+import Footer from '../components/Data/Footer.jsx'
 
 
 
@@ -11,8 +12,9 @@ const Home = () => {
   return (
     <div>
        <HeroBanner/>
-        <MultiBanner/>
-        <ShopItems/>
+       <MultiBanner/>
+       <ShopItems/>
+       <Footer/>
     </div>
   )
 }
---
+++ src/components/Data/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div class="max-w-[98%] mx-auto grid grid-cols-2 md:grid-cols-[25%_25%_25%_25%] lg:grid-cols-[25%_30%_25%_20%]">
+  <div class="mt-2 border-2 border-solid border-amber-400 mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum adipisci quaerat tempore velit, eveniet recusandae sequi quidem expedita qui?</div>
+  <div class="mt-2 border-2 border-solid border-amber-400 mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum adipisci quaerat tempore velit, eveniet recusandae sequi quidem expedita qui?</div>
+  <div class="mt-2 border-2 border-solid border-amber-400 mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum adipisci quaerat tempore velit, eveniet recusandae sequi quidem expedita qui?</div>
+  <div class="mt-2 border-2 border-solid border-amber-400 mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum adipisci quaerat tempore velit, eveniet recusandae sequi quidem expedita qui?</div>
+</div>
+  )
+}
+
+export default Footer
